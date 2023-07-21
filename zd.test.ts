452cff2206b40f116f9b55eb9577b61d119c44bb
James Moore <james@restphone.com>
2023-07-21 16:47:15 -0700

    initial commit

---
+++ zd.test.ts
@@ -0,0 +1,25 @@
+import {
+    ZeroDevEthersProvider,
+    convertEthersSignerToAccountSigner,
+} from "@zerodevapp/sdk";
+import {ethers} from "ethers";
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(7).toBe(3);
+});
+
+test("can get a wallet address", async () => {
+    const randomWallet = ethers.Wallet.createRandom();
+
+    const provider = await ZeroDevEthersProvider.init("ECDSA", {
+        projectId: process.env.ZERODEV_PROJECT_ID,
+        owner: convertEthersSignerToAccountSigner(randomWallet),
+        opts: {
+            paymasterConfig: {
+                policy: "VERIFYING_PAYMASTER",
+            },
+        },
+    });
+    const zdSigner = provider.getAccountSigner();
+    const zdSignerAddress = await zdSigner.getAddress();
+});
